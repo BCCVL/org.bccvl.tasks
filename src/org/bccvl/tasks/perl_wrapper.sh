725429051c6db04db449303a572d69e50f21f5af
Gerhard Weis <g.weis@griffith.edu.au>
2014-05-26 11:36:15 +1000

    update perl_wrapper.sh to run without any setup made in .bashrc

--- src/org/bccvl/tasks/perl_wrapper.sh
+++ src/org/bccvl/tasks/perl_wrapper.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-echo "PATH before: $PERL5LIB"
 # setup local::lib
 eval "$(scl enable perl516 \\"perl -I$HOME/perl5/lib/perl5 -Mlocal::lib\\")"
-# add biodivers to path
+# add biodiverse to path
 export PERL5LIB="${PERL5LIB:+${PERL5LIB}:}$HOME/biodiverse/lib"
-exec perl "$1"
+exec scl enable perl516 "perl '$1'"
