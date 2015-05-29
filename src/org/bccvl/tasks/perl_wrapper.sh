a21fe1620b4442b8daceaa1a6a49b03ee9a23c30
Gerhard Weis <g.weis@griffith.edu.au>
2015-05-29 15:04:57 +1000

    capture list of installed perl pakcages and versions in log file

--- src/org/bccvl/tasks/perl_wrapper.sh
+++ src/org/bccvl/tasks/perl_wrapper.sh
@@ -3,4 +3,5 @@
 eval "$(scl enable perl516 \\"perl -I$HOME/perl5/lib/perl5 -Mlocal::lib\\")"
 # add biodiverse to path
 export PERL5LIB="${PERL5LIB:+${PERL5LIB}:}$HOME/biodiverse/lib"
+scl enable pert516 "cpan -l"
 exec scl enable perl516 "perl '$1'"
