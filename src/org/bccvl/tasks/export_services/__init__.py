14fdab589c1507105ce02bd13a1747a55f710e7c
Jan Hettenhausen <j.hettenhausen@griffith.edu.au>
2015-09-18 11:17:53 +1000

    merged upstream changes

--- src/org/bccvl/tasks/export_services/__init__.py
+++ src/org/bccvl/tasks/export_services/__init__.py
@@ -1,8 +1,18 @@
+from __future__ import absolute_import
+from org.bccvl.tasks.celery import app
 from .dropbox import export_dropbox
 from .googledrive import export_googledrive
 from .figshare import export_figshare
 
 
+
 def unsupported_service(zipurl, serviceid, context):
     raise NotImplementedError(
         "{} is currently not a supported service".format(serviceid))
+
+@app.task()
+def export_result(zipurl, serviceid, context):
+    zipurl = "http://127.0.0.1:8201{context}/resultdownload".format(**context)
+    export_func = globals().get("export_{}".format(serviceid), unsupported_service)
+    export_func(zipurl, serviceid, context)
+
