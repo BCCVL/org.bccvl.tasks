1009288c46d6475607e9b279c4dede4da5779b2e
Gerhard Weis <g.weis@griffith.edu.au>
2016-04-05 11:22:21 +1000

    quickfix tests for zipped occurrence datasets

--- src/org/bccvl/tasks/tests/test_mdextractor.py
+++ src/org/bccvl/tasks/tests/test_mdextractor.py
@@ -3,6 +3,7 @@ from pkg_resources import resource_filename
 import shutil
 import tempfile
 import unittest
+import zipfile
 
 import mock
 
@@ -41,12 +42,16 @@ class Test_csv_utf8(unittest.TestCase):
     @mock.patch('org.bccvl.movelib.protocol.ala._download_occurrence_by_lsid')
     def test_ala_utf8_move(self, mock_occur, mock_md):
         def fetch_occur_data(lsid, dest):
-            occur_file = os.path.join(dest, 'ala_occurrence.csv')
-            shutil.copyfile(resource_filename(__name__, 'data.csv'),
+            occur_file = os.path.join(dest, 'ala_occurrence.zip')
+            shutil.copyfile(resource_filename(__name__, 'data.zip'),
                             occur_file)
+            # FIXME: ala.py exploits side effect, zip is being created in _download_metadata_for_lsid, but other methods in the module rely on the enpacked zip being available
+            # FIXME: ala.py alse rezips inside _ala_postprocess again
+            with zipfile.ZipFile(occur_file) as z:
+                z.extractall(dest)
             return { 'url' : occur_file,
-                     'name': 'ala_occurrence.csv',
-                     'content_type': 'text/csv'}
+                     'name': 'ala_occurrence.zip',
+                     'content_type': 'application/zip'}
 
         def fetch_meta_data(lsid, dest):
             metadata_file = os.path.join(dest, 'ala_metadata.json')
@@ -66,7 +71,8 @@ class Test_csv_utf8(unittest.TestCase):
             self.assertEqual(mock_occur.call_count, 1)
             self.assertEqual(mock_md.call_count, 1)
             dl_list = os.listdir(tmpdir)
+            # FIXME: data should not be there
             self.assertEqual(set(dl_list),
-                             set(['ala_occurrence.csv', 'ala_dataset.json', 'ala_metadata.json']))
+                             set(['ala_occurrence.zip', 'ala_dataset.json', 'ala_metadata.json', 'data']))
         finally:
             shutil.rmtree(tmpdir)
