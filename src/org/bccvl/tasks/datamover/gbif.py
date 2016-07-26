9cd7ab4adeed452c1833cb07a0daaf74fda5f635
Gerhard Weis <g.weis@griffith.edu.au>
2016-07-26 20:27:04 +1000

    whitespace

--- src/org/bccvl/tasks/datamover/gbif.py
+++ src/org/bccvl/tasks/datamover/gbif.py
@@ -76,13 +76,14 @@ def pull_occurrences_from_gbif(lsid, dest_url, context):
         }
 
         # Add the number of occurrence records to the metadata
-        # To do: This is a hack. Any better solution.
+        # TODO: This is a hack. Any better solution.
         occurrence_csv_filename = os.path.join('data', 'gbif_occurrence.csv')
         if occurrence_csv_filename in item['filemetadata']:
             # FIXME: copy all occurrence metadata to zip level, for backwards compatibility... this should go away after we fully support 'layered' occurrence zips.
             for key in ('rows', 'headers', 'bounds'):  # what about 'species' ?
                 if key in item['filemetadata'][occurrence_csv_filename]['metadata']:
                     item['filemetadata'][key] = item['filemetadata'][occurrence_csv_filename]['metadata'][key]
+
 
         # move data file to destination and build data_url
         src = build_source('file://{}'.format(gbif_csv))
