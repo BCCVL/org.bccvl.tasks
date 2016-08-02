949deb8e05c2538631e9787f6d0b7abe827c08a9
Yong Liaw <y.liaw@griffith.edu.au>
2016-08-02 17:12:32 +1000

    BCCVLB-513: Update import occurence data from AEKOS

--- src/org/bccvl/tasks/datamover/aekos.py
+++ src/org/bccvl/tasks/datamover/aekos.py
@@ -35,12 +35,12 @@ def pull_occurrences_from_aekos(species, dest_url, context):
 
         # FIXME: below needs to be updated once we know what an occurrence dataset from aekos looks like
 
-        # open ala_dateset.json
+        # open aekos_dateset.json
         aekos_ds = json.load(open(os.path.join(tmpdir, 'aekos_dataset.json'), 'r'))
         # collect files inside ds per datatype
         files = dict(((f['dataset_type'], f) for f in aekos_ds['files']))
-        # read ala metadata from attribution file
-        aekos_md = json.load(open(files['attribution']['url'], 'r'))
+        # read aekos metadata from attribution file
+        aekos_md = json.load(open(files['attribution']['url'], 'r'))        
         aekos_csv = files['occurrence']['url']  # this is actually a zip file now
 
         # build bccvl metadata:
@@ -48,40 +48,27 @@ def pull_occurrences_from_aekos(species, dest_url, context):
             'genre': 'DataGenreSpeciesOccurrence',
             'categories': ['occurrence'],
             'species': {
-                'scientificName': traverse_dict(ala_md, 'classification/scientificName'),
-                'vernacularName': traverse_dict(ala_md, 'commonNames/0/nameString'),
-                'taxonID': traverse_dict(ala_md, 'classification/guid'),
-                'rank': traverse_dict(ala_md, 'classification/rank'),
-                'genus': traverse_dict(ala_md, 'classification/genus'),
-                'genusGuid': traverse_dict(ala_md, 'classification/genusGuid'),
-                'family': traverse_dict(ala_md, 'classification/family'),
-                'familyGuid': traverse_dict(ala_md, 'classification/familyGuid'),
-                'order': traverse_dict(ala_md, 'classification/order'),
-                'orderGuid': traverse_dict(ala_md, 'classification/orderGuid'),
-                'clazz': traverse_dict(ala_md, 'classification/clazz'),
-                'clazzGuid': traverse_dict(ala_md, 'classification/clazzGuid'),
-                'phylum': traverse_dict(ala_md, 'classification/phylum'),
-                'phylumGuid': traverse_dict(ala_md, 'classification/phylumGuid'),
-                'kingdom': traverse_dict(ala_md, 'classification/kingdom'),
-                'kingdomGuid': traverse_dict(ala_md, 'classification/kingdomGuid')
+                'scientificName': traverse_dict(aekos_md, '0/scientificName'),
+                'taxonID': traverse_dict(aekos_md, '0/id'),
+                'rank': 'species')
             },
         }
         # build item to import
         item = {
-            'title': ala_ds['title'],
-            'description': ala_ds['description'],
+            'title': aekos_ds['title'],
+            'description': aekos_ds['description'],
             'file': {
-                'url': 'file://{}'.format(ala_csv),  # local file url
+                'url': 'file://{}'.format(aekos_csv),  # local file url
                 'contenttype': 'application/zip',
-                'filename': os.path.basename(ala_csv)
+                'filename': os.path.basename(aekos_csv)
             },
             'bccvlmetadata': bccvlmd,
-            'filemetadata': extract_metadata(ala_csv, 'application/zip'),
+            'filemetadata': extract_metadata(aekos_csv, 'application/zip'),
         }
 
         # Add the number of occurrence records to the metadata
         # To do: This is a hack. Any better solution.
-        occurrence_csv_filename = os.path.join('data', 'ala_occurrence.csv')
+        occurrence_csv_filename = os.path.join('data', 'aekos_occurrence.csv')
         if occurrence_csv_filename in item['filemetadata']:
             # FIXME: copy all occurrence metadata to zip level, for backwards compatibility... this should go away after we fully support 'layered' occurrence zips.
             for key in ('rows', 'headers', 'bounds'):  # what about 'species' ?
@@ -89,21 +76,21 @@ def pull_occurrences_from_aekos(species, dest_url, context):
                     item['filemetadata'][key] = item['filemetadata'][occurrence_csv_filename]['metadata'][key]
 
         # move data file to destination and build data_url
-        src = build_source('file://{}'.format(ala_csv))
-        dst = build_destination(os.path.join(dest_url, os.path.basename(ala_csv)), app.conf.get('bccvl', {}))
+        src = build_source('file://{}'.format(aekos_csv))
+        dst = build_destination(os.path.join(dest_url, os.path.basename(aekos_csv)), app.conf.get('bccvl', {}))
         item['file']['url'] = dst['url']
         movelib.move(src, dst)
         # tell importer about new dataset (import it)
-        set_progress('RUNNING', 'Import ala data {0}'.format(lsid), None, context)
+        set_progress('RUNNING', 'Import aekos data {0}'.format(lsid), None, context)
         cleanup_job = import_cleanup_job(dest_url, context)
         import_job = import_ala_job([item], dest_url, context)
-        import_job.link_error(set_progress_job("FAILED", "Import of ala data failed {0}".format(lsid), None, context))
+        import_job.link_error(set_progress_job("FAILED", "Import of aekos data failed {0}".format(lsid), None, context))
         import_job.link_error(cleanup_job)
-        finish_job = set_progress_job("COMPLETED", 'ALA import {} complete'.format(lsid), None, context)
+        finish_job = set_progress_job("COMPLETED", 'AEKOS import {} complete'.format(lsid), None, context)
         (import_job | cleanup_job | finish_job).delay()
 
     except Exception as e:
-        set_progress('FAILED', 'Download {0} from ala: {1}'.format(lsid, e), None, context)
+        set_progress('FAILED', 'Download {0} from aekos: {1}'.format(lsid, e), None, context)
         import_cleanup(dest_url, context)
         LOG.error('Download from %s to %s failed: %s', src, dest_url, e)
     finally:
