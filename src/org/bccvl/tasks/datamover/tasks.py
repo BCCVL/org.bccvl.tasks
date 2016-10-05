ebc603c188ce636412c9e5f94f30ca6de032254c
Gerhard Weis <g.weis@griffith.edu.au>
2016-10-05 11:20:16 +1000

    whitespace

--- src/org/bccvl/tasks/datamover/tasks.py
+++ src/org/bccvl/tasks/datamover/tasks.py
@@ -24,7 +24,8 @@ def move(move_args, context):
     errmsgs = []
     for src, dest in move_args:
         try:
-            source = build_source(src, context['user']['id'], app.conf.get('bccvl', {}))
+            source = build_source(src, context['user']['id'],
+                                  app.conf.get('bccvl', {}))
             destination = build_destination(dest, app.conf.get('bccvl', {}))
             movelib.move(source, destination)
         except Exception as e:
@@ -53,17 +54,27 @@ def update_metadata(url, filename, contenttype, context):
         # Check that there are lon and lat columns
         # if upload is of type csv, we validate column names as well
         if contenttype == 'text/csv':
-            if 'headers' not in item['filemetadata'] or 'lat' not in item['filemetadata']['headers'] or 'lon' not in item['filemetadata']['headers']:
+            if ('headers' not in item['filemetadata']
+                    or 'lat' not in item['filemetadata']['headers']
+                    or 'lon' not in item['filemetadata']['headers']):
                 raise Exception("Missing 'lat'/'lon' column")
 
-        set_progress('RUNNING', 'Import metadata for {0}'.format(url), None, context)
+        set_progress('RUNNING',
+                     'Import metadata for {0}'.format(url),
+                     None, context)
 
         import_job = import_file_metadata_job([item], url, context)
-        import_job.link_error(set_progress_job("FAILED", "Metadata update failed for {0}".format(url), None, context))
-        finish_job = set_progress_job("COMPLETED", 'Metadata update for {} complete'.format(url), None, context)
+        import_job.link_error(set_progress_job(
+            "FAILED", "Metadata update failed for {0}".format(url),
+            None, context))
+        finish_job = set_progress_job(
+            "COMPLETED", 'Metadata update for {} complete'.format(url),
+            None, context)
         (import_job | finish_job).delay()
     except Exception as e:
-        set_progress('FAILED', 'Metadata update for {} failed: {}'.format(url, e), None, context)
+        set_progress('FAILED',
+                     'Metadata update for {} failed: {}'.format(url, e),
+                     None, context)
         LOG.error('Metadata update for %s failed: %s', url, e)
     finally:
         if tmpdir and os.path.exists(tmpdir):
@@ -91,25 +102,33 @@ def import_multi_species_csv(url, results_dir, import_context, context):
 
         # Check that there are lon and lat columns
         # if upload is of type csv, we validate column names as well
-        if 'headers' not in item['filemetadata'] or 'lat' not in item['filemetadata']['headers'] or 'lon' not in item['filemetadata']['headers']:
+        if ('headers' not in item['filemetadata']
+                or 'lat' not in item['filemetadata']['headers']
+                or 'lon' not in item['filemetadata']['headers']):
             raise Exception("Missing 'lat'/'lon' column")
 
-        set_progress('RUNNING', 'Import metadata for {0}'.format(url), None, context)
+        set_progress('RUNNING',
+                     'Import metadata for {0}'.format(url),
+                     None, context)
 
         import_md_job = import_file_metadata_job([item], url, context)
-        import_md_job.link_error(set_progress_job("FAILED", "Metadata update failed for {0}".format(url), None, context))
+        import_md_job.link_error(set_progress_job(
+            "FAILED", "Metadata update failed for {0}".format(url),
+            None, context))
 
         # step 2: split csv file and create sub datasets
-        # start reading csv file and create new datasets which will be linked up with dataset collection item
-        # FIXME: large csv files should be streamed to seperate files (not read into ram like here)
-        f = io.open(tmpfile, 'r')
+        # start reading csv file and create new datasets which will be
+        #       linked up with dataset collection item
+        # FIXME: large csv files should be streamed to seperate files (not read
+        #        into ram like here)
         f = io.open(tmpfile, 'rb')
         csvreader = csv.reader(f)
         headers = csvreader.next()
         if 'species' not in headers:
             raise Exception('missing species column')
         speciesidx = headers.index('species')
-        # create dict with all data .... species column used as key, and rest is just added
+        # create dict with all data .... species column used as key, and rest
+        # is just added
         data = {}
         for row in csvreader:
             if not row:
@@ -145,11 +164,14 @@ def import_multi_species_csv(url, results_dir, import_context, context):
         # send files to destination
         for species in data:
             src = build_source('file://{}'.format(data[species]['path']))
-            dst = build_destination(os.path.join(results_dir, data[species]['name']), app.conf.get('bccvl', {}))
+            dst = build_destination(os.path.join(results_dir,
+                                                 data[species]['name']),
+                                    app.conf.get('bccvl', {}))
             data[species]['url'] = dst['url']
             movelib.move(src, dst)
         # all files uploaded .... send import jobs
-        set_progress('RUNNING', 'Create datasets for {0}'.format(url), None, context)
+        set_progress('RUNNING', 'Create datasets for {0}'.format(
+            url), None, context)
         items = []
         for species in data:
             # build item
@@ -177,18 +199,25 @@ def import_multi_species_csv(url, results_dir, import_context, context):
             }
             items.append(item)
         # start import process
-        start_import = set_progress_job('RUNNING', 'Import results', None, context)
-        ########### What is results_dir being used for?
+        start_import = set_progress_job(
+            'RUNNING', 'Import results', None, context)
+        # What is results_dir being used for?
         import_job = import_result_job(items, results_dir, import_context)
         cleanup_job = import_cleanup_job(results_dir, context)
-        import_job.link_error(set_progress_job('FAILED', 'Multi species import failed', None, context))
+        import_job.link_error(set_progress_job(
+            'FAILED', 'Multi species import failed', None, context))
         import_job.link_error(cleanup_job)
-        finish_job = set_progress_job('COMPLETED', 'Task succeeded', None, context)
-        (start_import | import_md_job | import_job | cleanup_job | finish_job).delay()
+        finish_job = set_progress_job(
+            'COMPLETED', 'Task succeeded', None, context)
+        (start_import | import_md_job | import_job |
+         cleanup_job | finish_job).delay()
         # FIXME: missing stuff...
         #        need to set multi species collection to finished at some stage
     except Exception as e:
-        set_progress('FAILED', 'Error while splitting Multi Species CSV {}: {}'.format(url, e), None, context)
+        set_progress('FAILED',
+                     'Error while splitting Multi Species CSV {}: {}'.format(
+                         url, e),
+                     None, context)
         LOG.error('Multi species split for %s faild: %s', url, e)
     finally:
         if tmpdir and os.path.exists(tmpdir):
