8b9884e0c535e7059204daf68f73e448d3f32fc8
Gerhard Weis <g.weis@griffith.edu.au>
2017-05-25 08:14:15 +1000

    update perl wrapper to support non scl environments

--- src/org/bccvl/tasks/perl_wrapper.sh
+++ src/org/bccvl/tasks/perl_wrapper.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
-# setup local::lib
-eval "$(scl enable perl516 \\"perl -I$HOME/perl5/lib/perl5 -Mlocal::lib\\")"
+# check if perl is from scl
+SCL=$(which scl 2>/dev/null)
+if [ -n "$SCL" ] ; then
+    # we have SCL
+    if $SCL -l | grep -q perl516 ; then
+        # and perl516
+        # setup local::lib
+        eval "$(scl enable perl516 \\"perl -I$HOME/perl5/lib/perl5 -Mlocal::lib\\")"
+        # add biodiverse to path
+        export PERL5LIB="${PERL5LIB:+${PERL5LIB}:}$HOME/biodiverse/lib"
+        # print out installed packages and versions
+        #scl enable pert516 "cpan -l"
+        # replace this process with perl
+        exec scl enable perl516 "perl '$1'"
+    fi
+fi
+# no scl or perl516 in scl
 # add biodiverse to path
-export PERL5LIB="${PERL5LIB:+${PERL5LIB}:}$HOME/biodiverse/lib"
-scl enable pert516 "cpan -l"
-exec scl enable perl516 "perl '$1'"
+export PERL5LIB="${PERL5LIB:+${PERL5LIB}:}/opt/biodiverse-r1.0/lib"
+exec perl "$1"
