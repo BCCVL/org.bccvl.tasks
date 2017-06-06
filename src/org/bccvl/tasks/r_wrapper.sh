658e7cc4ced4134e57199f00cc86cbf6a6a4c73a
Gerhard Weis <g.weis@griffith.edu.au>
2017-06-06 22:04:00 +1000

    fix r wrapper for non docker env

--- src/org/bccvl/tasks/r_wrapper.sh
+++ src/org/bccvl/tasks/r_wrapper.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-mkdir -p "${WORKDIR}/R_LIBS"
-export R_LIBS="${WORKDIR}/R_LIBS"
+# FIXME: these env vars work only in docker env
+#mkdir -p "${WORKDIR}/R_LIBS"
+#export R_LIBS="${WORKDIR}/R_LIBS"
 
 exec Rscript --no-save --no-restore $1
