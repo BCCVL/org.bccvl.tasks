14f5b87efe551cdee144e1bbb6fa46ea25bb9cf3
Gerhard Weis <g.weis@griffith.edu.au>
2017-06-06 22:08:44 +1000

    support docker and non docker deployment

--- src/org/bccvl/tasks/r_wrapper.sh
+++ src/org/bccvl/tasks/r_wrapper.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+
 # FIXME: these env vars work only in docker env
-#mkdir -p "${WORKDIR}/R_LIBS"
-#export R_LIBS="${WORKDIR}/R_LIBS"
+if [  -z "$R_LIBS" ] ; then
+    # if R_LIBS is not set, use workdir for users r libs
+    mkdir -p "${WORKDIR}/R_LIBS"
+    export R_LIBS="${WORKDIR}/R_LIBS"
+fi
 
 exec Rscript --no-save --no-restore $1
